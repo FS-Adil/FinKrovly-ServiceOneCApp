a7a05aa4dd7335ae1a1ccfbe73e26b5e6bd9a228
FinKrovly-ServiceOneCApp <adildisel@mail.ru>
2026-01-16 16:13:30 +0300

    add project, add start settings

---
+++ backendserviceonecapp/serviceonec/src/main/java/com/example/serviceonec/ServiceonecApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceonec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceonecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceonecApplication.class, args);
+	}
+
+}
---
+++ backendserviceonecapp/serviceonec/src/test/java/com/example/serviceonec/ServiceonecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceonec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceonecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
