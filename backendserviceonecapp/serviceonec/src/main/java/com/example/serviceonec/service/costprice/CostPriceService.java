39908dddf38bc432a2ade978e5304dc39eab6de4
FinKrovly-ServiceOneCApp <adildisel@mail.ru>
2026-02-19 16:09:23 +0300

    Перебрал расчет себестоимости, добавил уменьшение приходников в зависимости от остатков в организации

--- backendserviceonecapp/serviceonec/src/main/java/com/example/serviceonec/service/costprice/CostPriceService.java
+++ backendserviceonecapp/serviceonec/src/main/java/com/example/serviceonec/service/costprice/CostPriceService.java
@@ -3,7 +3,8 @@ package com.example.serviceonec.service.costprice;
 import com.example.serviceonec.controller.costprice.output.CostPriceControllerOutput;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CostPriceService {
-    List<CostPriceControllerOutput> getAllCostPrice();
+    List<CostPriceControllerOutput> getAllCostPrice(UUID organizationId);
 }
